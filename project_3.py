094396ec851d023df272ba4639c6996c274ff1b4
FEI YANG <fainyang@foxmail.com>
2017-09-19 11:09:17 +0800

    add_delete
    add time and delete function

--- project_3.py
+++ project_3.py
@@ -3,8 +3,12 @@ def repeat(L):   #去重
 	L2=list(set(L))
 	L2.sort(key=L.index)
 	return L2
+#删除功能，待完成
+def delete(y_):
+	for i in range(Row):
+		del list_2d[i][y_]
 
-rf = open('2017-09-07.csv','r') 
+rf = open('2017-09-02.csv','r') 
 reader = csv.reader(rf)
 test=open('info.csv','w')
 writer=csv.writer(test)
@@ -16,17 +20,23 @@ for row in reader:
 
 Mac=repeat(Mac)
 time=repeat(time)
+
 time.insert(1,'individual_time')
 Mac.insert(0,'TIME')
 Mac.insert(1,len(Mac))
-row=len(time)+2
-line=len(Mac)+2
+line1=len(Mac)
 
-list_2d=[['' for i in range(line)] for i in range(row)] #创建二维list
+for i in range(2,2*line1,2):
+	Mac.insert(i,'first\last')
+
+Row=len(time)
+line=len(Mac)
+
+list_2d=[['' for i in range(line)] for i in range(Row)] #创建二维list
 list_2d[0]=Mac     #mac放进2维数据
 list_2d[1]=[0 for i in range(line)]
-n=0
 
+n=0
 for i in time:    #时间放进2维数组
 	
 	list_2d[n][0]=i
@@ -34,26 +44,31 @@ for i in time:    #时间放进2维数组
 	n=n+1
 
 
-
-
 rf.seek(0)
 for row in reader:
 	x=time.index(row[0])
 	y=Mac.index(row[1])
-	location=row[16]+'\\'+row[17]
+	location=row[16]+'\\'+row[17]  
+	fltime=row[14][5:19]+'\\'+row[15][5:19]
 	list_2d[x][y]=location
+	list_2d[x][y+1]=fltime
 	count=int(list_2d[x][1])+1
 	list_2d[x][1]=count 
-	
-	in_time=int(list_2d[1][y])+1
+	if x<98:
+		in_time=int(list_2d[1][y])+5
+	else:
+		in_time=int(list_2d[1][y])+1
 	list_2d[1][y]=in_time
 
+list_2d[0][1]=line1-3
 
+for i in range(3):
+	delete(2)
 
 
 for row in list_2d:
 	writer.writerow(row)
-	
+
 
 rf.close()
 test.close()
